89f145cb14b9ab85c5800910397957353540c150
Erik Reed <ereed.uw@gmail.com>
2012-01-05 14:40:46 -0800

    formatting; spaces instead of tabs

--- OptMLP.hpp
+++ OptMLP.hpp
@@ -16,58 +16,58 @@
 const double ETA = .2; // weight coefficient
 const size_t MAX_ITERATIONS = 15;
 
-template <class T=double>
+template<class T = double>
 class Matrix {
 protected:
-	T *dat;
+    T *dat;
 public:
-	const size_t rows;
-	const size_t cols;
+    const size_t rows;
+    const size_t cols;
 
-	Matrix(const size_t rows, const size_t cols) : rows(rows), cols(cols) {
-		dat = new T[rows*cols];
-	}
+    Matrix(const size_t rows, const size_t cols) :
+            rows(rows), cols(cols) {
+        dat = new T[rows * cols];
+    }
 
-	Matrix(const Matrix<T> &m) {
-		this(m.rows,m.cols);
-		for (size_t i=0; i<rows*cols; i++)
-			dat[i] = m.dat[i];
-	}
+    Matrix(const Matrix<T> &m) {
+        this(m.rows, m.cols);
+        for (size_t i = 0; i < rows * cols; i++)
+            dat[i] = m.dat[i];
+    }
 
-	inline
-	void set(const size_t row, const size_t col, const T &val) {
-		dat[cols*row + col] = val;
-	}
+    inline
+    void set(const size_t row, const size_t col, const T &val) {
+        dat[cols * row + col] = val;
+    }
 
-	inline
-	T get(const size_t row, const size_t col) {
-		return dat[cols*row + col];
-	}
+    inline T get(const size_t row, const size_t col) {
+        return dat[cols * row + col];
+    }
 
-	void randomize() {
-		randomize(1);
-	}
+    void randomize() {
+        randomize(1);
+    }
 
-	void randomize(double normalize) {
-		srand (time(NULL));
-		for (size_t i=0; i<rows*cols; i++) {
-			dat[i] = (rand() / RAND_MAX - .5)*2/normalize;
-		}
-	}
+    void randomize(double normalize) {
+        srand(time(NULL));
+        for (size_t i = 0; i < rows * cols; i++) {
+            dat[i] = (rand() / RAND_MAX - .5) * 2 / normalize;
+        }
+    }
 
-	~Matrix() {
-		delete[] dat;
-	}
+    ~Matrix() {
+        delete[] dat;
+    }
 
-	friend std::ostream& operator<<(std::ostream& cout, Matrix& m) {
-		for (size_t i=0; i<m.rows*m.cols; i++) {
-			if (i%m.cols == 0 && i!=0)
-				cout << std::endl;
-			cout << m.dat[i] << " ";
-		}
-		cout << std::endl;
-		return cout;
-	}
+    friend std::ostream& operator<<(std::ostream& cout, Matrix& m) {
+        for (size_t i = 0; i < m.rows * m.cols; i++) {
+            if (i % m.cols == 0 && i != 0)
+                cout << std::endl;
+            cout << m.dat[i] << " ";
+        }
+        cout << std::endl;
+        return cout;
+    }
 
 };
 
--- main.cpp
+++ main.cpp
@@ -13,74 +13,76 @@ using namespace std;
 
 // single layer perceptron with threshold activation function
 void SLP(Matrix<double> &in, Matrix<double> &out) {
-	size_t numInputs = in.cols; // length of input vector
-	size_t numVectors = in.rows; // i.e. numTargets
-	size_t numOutputs = out.cols; // i.e. length of target vector
+    size_t numInputs = in.cols; // length of input vector
+    size_t numVectors = in.rows; // i.e. numTargets
+    size_t numOutputs = out.cols; // i.e. length of target vector
 
-	if (in.rows != out.rows)
-		throw "number of test cases different between in/out...";
+    if (in.rows != out.rows)
+        throw "number of test cases different between in/out...";
 
-	// num neurons by num weights (+1 for input bias)
-	Matrix<double> weights(numOutputs,numInputs+1);
-	weights.randomize(20);
+    // num neurons by num weights (+1 for input bias)
+    Matrix<double> weights(numOutputs, numInputs + 1);
+    weights.randomize(20);
 
-	char *activation = new char[numOutputs];
-	for (size_t iter=0; iter<MAX_ITERATIONS; iter++) {
-		double diff = 0;
-		for (size_t i=0; i<numVectors; i++) {
-			// compute activations
-			for (size_t j=0; j<numOutputs; j++) {
-				double sum = 0;
-				for (size_t k=0; k<numInputs; k++)
-					sum += weights.get(j,k) * in.get(i,k);
-				sum += weights.get(j,numInputs) * -1; // input bias
-				activation[j] = sum > 0 ? 1 : 0;
-			}
-			// update weights
-			for (size_t j=0; j<numOutputs; j++) {
-				for (size_t k=0; k<numInputs+1; k++) {
-					double oldWeight = weights.get(j,k);
-					double newWeight = oldWeight + ETA*(out.get(i,j)-activation[j])*in.get(i,k);
-					diff += oldWeight - newWeight;
-					weights.set(j,k,newWeight);
-				}
-			}
+    char *activation = new char[numOutputs];
+    for (size_t iter = 0; iter < MAX_ITERATIONS; iter++) {
+        double diff = 0;
+        for (size_t i = 0; i < numVectors; i++) {
+            // compute activations
+            for (size_t j = 0; j < numOutputs; j++) {
+                double sum = 0;
+                for (size_t k = 0; k < numInputs; k++)
+                    sum += weights.get(j, k) * in.get(i, k);
+                sum += weights.get(j, numInputs) * -1; // input bias
+                activation[j] = sum > 0 ? 1 : 0;
+            }
+            // update weights
+            for (size_t j = 0; j < numOutputs; j++) {
+                for (size_t k = 0; k < numInputs + 1; k++) {
+                    double oldWeight = weights.get(j, k);
+                    double newWeight = oldWeight
+                            + ETA * (out.get(i, j) - activation[j])
+                                    * in.get(i, k);
+                    diff += oldWeight - newWeight;
+                    weights.set(j, k, newWeight);
+                }
+            }
 
-		}
-		cout << "Iteration: " << iter << "\tdiff: " << diff << endl;
-		cout << weights << endl;
-		if (diff==0) //converged
-			break;
-	}
-	delete[] activation;
+        }
+        cout << "Iteration: " << iter << "\tdiff: " << diff << endl;
+        cout << weights << endl;
+        if (diff == 0) //converged
+            break;
+    }
+    delete[] activation;
 }
 
 void tests() {
-	Matrix<double> test1(4,2);
-	Matrix<double> test1out(4,1);
+    Matrix<double> test1(4, 2);
+    Matrix<double> test1out(4, 1);
 
-	// OR input
-	// TODO: more optimal setting
-	test1.set(0,0,0);
-	test1.set(0,1,0);
-	test1.set(1,0,1);
-	test1.set(1,1,0);
-	test1.set(2,0,0);
-	test1.set(2,1,1);
-	test1.set(3,0,1);
-	test1.set(3,1,1);
+    // OR input
+    // TODO: more optimal setting
+    test1.set(0, 0, 0);
+    test1.set(0, 1, 0);
+    test1.set(1, 0, 1);
+    test1.set(1, 1, 0);
+    test1.set(2, 0, 0);
+    test1.set(2, 1, 1);
+    test1.set(3, 0, 1);
+    test1.set(3, 1, 1);
 
-	test1out.set(0,0,0);
-	test1out.set(1,0,1);
-	test1out.set(2,0,1);
-	test1out.set(3,0,1);
+    test1out.set(0, 0, 0);
+    test1out.set(1, 0, 1);
+    test1out.set(2, 0, 1);
+    test1out.set(3, 0, 1);
 
-	SLP(test1,test1out);
+    SLP(test1, test1out);
 }
 
-int main(int argc, char** args){
+int main(int argc, char** args) {
 
-	tests();
-	return 0;
+    tests();
+    return 0;
 }
 
