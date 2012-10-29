ab41d59b2578e89b47b4b774537e568538fd81b9
erikreed <ereed.uw@gmail.com>
2012-10-28 23:01:01 -0400

    hacky stochastic gradient descent with L1 reg

--- regularization.m
+++ regularization.m
@@ -1,3 +1,5 @@
+%% Erik Reed (erikreed@cmu.edu)
+
 training = importdata('d2-train.csv');
 Y = training(:,1);
 X = training(:,2:end);
@@ -19,11 +21,11 @@ Xt = [ones(size(Xt, 1), 1) Xt];
 % P(y|x) = u(x)^y(1-u(x))^(1-y)
 
 % training
-maxLambda = 50;
-lambdaErr = zeros(1,maxLambda+1);
-for lambda=0:maxLambda
+maxLambda = 19;
+lambdaErr = zeros(1,maxLambda);
+for lambda=1:maxLambda
     classified = zeros(length(Y),1);
-    for k=1:size(X,1)
+    %
         theta = zeros(1,305);
         delta = inf;
         steps = 0;
@@ -31,26 +33,26 @@ for lambda=0:maxLambda
             steps = steps + 1;
             theta_old = theta;
             for j=1:size(X,1)
-                theta = theta + STEP_SIZE * ...
-                    (Y(j) - 1/(1+exp(-theta*X(j,:)')))*X(j,:) * ...
-                    (lambda / (2 * size(X,1)));
+                theta_diff = STEP_SIZE * ...
+                    (Y(j) - 1/(1+exp(-theta*X(j,:)')))*X(j,:);
+                theta = theta + theta_diff - abs(theta_diff) * lambda / size(X,1);
             end
 
             delta = sqrt(sum((theta_old - theta).^2));
         end
-        
+        for k=1:50
         classified(k) = round(1/(1+exp(-theta*X(k,:)')));
 
     end
     
     trainingError = sum(classified ~= Y) / length(Y);
-    lambdaErr(lambda + 1) = trainingError;
+    lambdaErr(lambda) = trainingError;
 end
 
 % testing
-% classified = zeros(size(Xt, 1), 1);
-% for i=1:size(Xt, 1)
-%     classified(i) = round(1/(1+exp(-theta*Xt(i,:)')));
-% end
-% testError = sum(classified ~= Yt) / length(Yt);
-% 
+classified = zeros(size(Xt, 1), 1);
+for i=1:size(Xt, 1)
+    classified(i) = round(1/(1+exp(-theta*Xt(i,:)')));
+end
+testError = sum(classified ~= Yt) / length(Yt);
+
